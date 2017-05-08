9031613e0ec1d48cea16ef60a8b59182b07a6ab6
Michael E Miles <michael@miles-me.net>
2017-05-08 03:11:57 -0500

    Remove using statements and comments

--- health/Program.cs
+++ health/Program.cs
@@ -1,27 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace health
 {
-    class Program
+    internal static class Program
     {
-        static void Main()
+        private static void Main()
         {
-            //Create new random class
             var rnd = new Random();
 
-            //Initialize variables
             var health = 50;
             var healthPotion = rnd.Next(25, 51);
             var difficulty = 1;
 
-            //Add healthPotion  to health
             health = health + healthPotion / difficulty;
 
-            //Print your new health
             Console.WriteLine(health);
             Console.ReadLine();
         }
